32ae1a9e9905fac9e797408d7cd8aedd09b326d8
endalk <endalkhbel@gmail.com>
2020-02-27 11:02:14 +0300

    sync contact

--- lib/pages/contact_sync/contact_sync.dart
+++ lib/pages/contact_sync/contact_sync.dart
@@ -90,7 +90,7 @@ class _ContactListPageState extends State<ContactListPage> {
                   ? CircleAvatar(backgroundImage: MemoryImage(c.avatar))
                   : CircleAvatar(child: Text(c.initials())),
               title: Text(c.displayName ?? ""),
-              subtitle: Text(c.phones??""),
+//              subtitle: Text(c??""),
 
             );
           },
@@ -101,141 +101,104 @@ class _ContactListPageState extends State<ContactListPage> {
   }
 }
 
-class ContactDetailsPage extends StatelessWidget {
-  ContactDetailsPage(this._contact);
-  final Contact _contact;
+class AddContactPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _AddContactPageState();
+}
+
+class _AddContactPageState extends State<AddContactPage> {
+  Contact contact = Contact();
+  PostalAddress address = PostalAddress(label: "Home");
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_contact.displayName ?? ""),
+        title: Text("Add a contact"),
         actions: <Widget>[
-//          IconButton(
-//            icon: Icon(Icons.share),
-//            onPressed: () => shareVCFCard(context, contact: _contact),
-//          ),
-          IconButton(
-            icon: Icon(Icons.delete),
-            onPressed: () => ContactsService.deleteContact(_contact),
-          ),
-          IconButton(
-            icon: Icon(Icons.update),
+          FlatButton(
             onPressed: () {
-
-            }
-//
-//                Navigator.of(context).push(
-//              MaterialPageRoute(builder: (context) => UpdateContactsPage(contact: _contact,),),
-//            ),
-          ),
+              _formKey.currentState.save();
+              contact.postalAddresses = [address];
+              ContactsService.addContact(contact);
+              Navigator.of(context).pop();
+            },
+            child: Icon(Icons.save, color: Colors.white),
+          )
         ],
       ),
-      body: SafeArea(
-        child: ListView(
-          children: <Widget>[
-            ListTile(
-              title: Text("Name"),
-              trailing: Text(_contact.givenName ?? ""),
-            ),
-            ListTile(
-              title: Text("Middle name"),
-              trailing: Text(_contact.middleName ?? ""),
-            ),
-            ListTile(
-              title: Text("Family name"),
-              trailing: Text(_contact.familyName ?? ""),
-            ),
-            ListTile(
-              title: Text("Prefix"),
-              trailing: Text(_contact.prefix ?? ""),
-            ),
-            ListTile(
-              title: Text("Suffix"),
-              trailing: Text(_contact.suffix ?? ""),
-            ),
-            ListTile(
-              title: Text("Company"),
-              trailing: Text(_contact.company ?? ""),
-            ),
-            ListTile(
-              title: Text("Job"),
-              trailing: Text(_contact.jobTitle ?? ""),
-            ),
-            AddressesTile(_contact.postalAddresses),
-            ItemsTile("Phones", _contact.phones),
-            ItemsTile("Emails", _contact.emails)
-          ],
+      body: Container(
+        padding: EdgeInsets.all(12.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'First name'),
+                onSaved: (v) => contact.givenName = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Middle name'),
+                onSaved: (v) => contact.middleName = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Last name'),
+                onSaved: (v) => contact.familyName = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Prefix'),
+                onSaved: (v) => contact.prefix = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Suffix'),
+                onSaved: (v) => contact.suffix = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Phone'),
+                onSaved: (v) => contact.phones = [Item(label: "mobile", value: v)],
+                keyboardType: TextInputType.phone,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'E-mail'),
+                onSaved: (v) => contact.emails = [Item(label: "work", value: v)],
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Company'),
+                onSaved: (v) => contact.company = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Job'),
+                onSaved: (v) => contact.jobTitle = v,
+              ),
+
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Street'),
+                onSaved: (v) => address.street = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'City'),
+                onSaved: (v) => address.city = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Region'),
+                onSaved: (v) => address.region = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Postal code'),
+                onSaved: (v) => address.postcode = v,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Country'),
+                onSaved: (v) => address.country = v,
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class AddressesTile extends StatelessWidget {
-  AddressesTile(this._addresses);
-  final Iterable<PostalAddress> _addresses;
 
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ListTile(title: Text("Addresses")),
-        Column(
-          children: _addresses.map((a) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  title: Text("Street"),
-                  trailing: Text(a.street ?? ""),
-                ),
-                ListTile(
-                  title: Text("Postcode"),
-                  trailing: Text(a.postcode ?? ""),
-                ),
-                ListTile(
-                  title: Text("City"),
-                  trailing: Text(a.city ?? ""),
-                ),
-                ListTile(
-                  title: Text("Region"),
-                  trailing: Text(a.region ?? ""),
-                ),
-                ListTile(
-                  title: Text("Country"),
-                  trailing: Text(a.country ?? ""),
-                ),
-              ],
-            ),
-          )).toList(),
-        ),
-      ],
-    );
-  }
-}
-
-class ItemsTile extends StatelessWidget {
-  ItemsTile(this._title, this._items);
-  final Iterable<Item> _items;
-  final String _title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        ListTile(title: Text(_title)),
-        Column(
-          children: _items.map((i) => Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ListTile(
-              title: Text(i.label ?? ""),
-              trailing: Text(i.value ?? ""),
-            ),),
-          ).toList(),
-        ),
-      ],
-    );
-  }
-}
