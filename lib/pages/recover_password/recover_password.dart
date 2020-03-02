0a7790a8f30ede33775ae18042271347ec3bf637
Yohannes Berhanu <Johnlights51@gmail.com>
2020-03-02 12:59:49 +0300

    forgot password link

--- lib/pages/recover_password/recover_password.dart
+++ lib/pages/recover_password/recover_password.dart
@@ -8,7 +8,6 @@ import 'package:flutter_app/pages/widgets/circularProgressBar.dart';
 import 'package:flutter_app/state/app_state.dart';
 import 'package:flutter_app/utilities/validation/get_size.dart';
 import 'package:provider/provider.dart';
-
 import '../../utilities/validation/Validation.dart';
 
 class RecoverPassword extends StatefulWidget {
@@ -56,7 +55,6 @@ class _RecoverPassword extends State<RecoverPassword> {
             ? Text(emailError, style: TextStyle(color: Colors.red))
             : Container(),
       ],
-
     );
   }
 
@@ -84,68 +82,64 @@ class _RecoverPassword extends State<RecoverPassword> {
   resetPassword() {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-            Text("Reset your Password",
-            style: TextStyle(
-              fontWeight: FontWeight.w400,
-              fontSize: 26
-            ),),
-        Card(
+        child: Column(children: [
+          Text(
+            "Reset your Password",
+            style: TextStyle(fontWeight: FontWeight.w400, fontSize: 26),
+          ),
+          Card(
             shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(20.0),
-  ),
-          child: Column(
-            children: [
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  children: [
-                    Container(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Text(
-                        Constant.PASSWORDRESETDESC,
-                        style: TextStyle(
-                          fontSize: 16,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(bottom: 10),
+                        child: Text(
+                          Constant.PASSWORDRESETDESC,
+                          style: TextStyle(
+                            fontSize: 16,
+                          ),
                         ),
                       ),
-                    ),
-                    emailTextFormField(),
-                  ],
+                      emailTextFormField(),
+                    ],
+                  ),
                 ),
-              ),
 
-              Padding(
-                padding: EdgeInsets.only(left: 25, right: 20),
-                child: message.isNotEmpty == true
-                    ? Text(message, style: TextStyle(color: Colors.green))
-                    : Container(),
-              ),
+                Padding(
+                  padding: EdgeInsets.only(left: 25, right: 20),
+                  child: message.isNotEmpty == true
+                      ? Text(message, style: TextStyle(color: Colors.green))
+                      : Container(),
+                ),
 
-              Consumer<Auth>(
-                builder: (BuildContext context, Auth value, Widget child) =>
-                    value.getHasErrorFun().toString().isNotEmpty == true
-                        ? Text(value.getHasErrorFun(),
-                            style: TextStyle(color: Colors.red))
-                        : Container(),
-              ),
-              // SizedBox(height: 15,),
-              Container(
-                width: 200,
-                child: button(),
-              ),
-              SizedBox(
-                height: 10,
-              )
-            ],
+                Consumer<Auth>(
+                  builder: (BuildContext context, Auth value, Widget child) =>
+                      value.getHasErrorFun().toString().isNotEmpty == true
+                          ? Text(value.getHasErrorFun(),
+                              style: TextStyle(color: Colors.red))
+                          : Container(),
+                ),
+                // SizedBox(height: 15,),
+                Container(
+                  width: 200,
+                  child: button(),
+                ),
+                SizedBox(
+                  height: 10,
+                )
+              ],
+            ),
           ),
-        ),
-          ]
-        ),
-
+        ]),
         margin: EdgeInsets.all(20),
       ),
     );
@@ -170,20 +164,18 @@ class _RecoverPassword extends State<RecoverPassword> {
     if (emailError.isEmpty && showError) {
       Provider.of<Auth>(context, listen: false).setLoadingStateFun(true);
 
-      var reset = resetPasswordApi(
-          email: emailController.text
-      );
+      var reset = resetPasswordApi(email: emailController.text);
       reset.then((val) {
         Provider.of<Auth>(context, listen: false).setLoadingStateFun(false);
         setState(() {
           message =
-          "A password reset email has been sent. Please check your email.";
+              "A password reset link has been sent. Please check your email.";
         });
       });
       reset.catchError((val) {
         Provider.of<Auth>(context, listen: false).setLoadingStateFun(false);
-        Provider.of<Auth>(context, listen: false).setHasErrorFun(
-            val.toString());
+        Provider.of<Auth>(context, listen: false)
+            .setHasErrorFun(val.toString());
         setState(() {
           message = "";
         });
